2f5fe37197621eb1b2ca9ebc2c33439d16fa7b94
pablo99nunez <pablo99nunez@gmail.com>
2021-11-01 19:36:58 -0300

    pablo 02 tipos de funciones

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ const queEs = document.getElementById("queEs");
 const infoQueEs = document.getElementById("infoQueEs")
 const imagen = document.getElementById("printer2")
 
+const cards = document.getElementsByClassName("card");
+const infos = document.getElementsByClassName("card-info");
+const closeButton = document.getElementsByClassName("close");
+
+for(let i=0;i<cards.length;i++){
+    cards[i].addEventListener("click",(e)=>{
+        infos[i].classList.remove("disable");
+        closeButton[i].addEventListener("click",()=>{
+            infos[i].classList.add("disable");
+        })
+    })
+}
